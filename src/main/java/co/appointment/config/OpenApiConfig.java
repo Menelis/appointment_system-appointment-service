cb019716f2278e0e49f447028b8ef5346a52c738
Menelisi Mthembu <menelismthembu@gmail.com>
2025-11-17 18:47:06 +0200

    Added @EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)

--- src/main/java/co/appointment/config/OpenApiConfig.java
+++ src/main/java/co/appointment/config/OpenApiConfig.java
@@ -4,8 +4,10 @@ import co.appointment.shared.util.OpenApiUtils;
 import io.swagger.v3.oas.models.OpenAPI;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @Configuration
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenAPI(final AppConfigProperties appConfigProperties) {
