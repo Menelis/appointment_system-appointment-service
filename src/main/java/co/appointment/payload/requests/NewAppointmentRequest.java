6f7065e04acc3a0d63f09ffaca631c1144795eab
Menelisi Mthembu <menelismthembu@gmail.com>
2025-11-17 18:45:07 +0200

    Updated property for AppointmentDate to Date

--- src/main/java/co/appointment/payload/requests/NewAppointmentRequest.java
+++ src/main/java/co/appointment/payload/requests/NewAppointmentRequest.java
@@ -3,15 +3,13 @@ package co.appointment.payload.requests;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
-import java.time.LocalDate;
+import java.sql.Date;
 
 @Data
 public class NewAppointmentRequest {
     private Integer branchId;
-    private LocalDate appointmentDate;
+    private Date appointmentDate;
     private Integer slotId;
     @Size(max = 500, message = "Only 500 characters are allowed for appointment description")
     private String description;
-
-
 }
