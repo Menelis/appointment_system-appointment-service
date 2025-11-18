878d6cd0a8ab339dc614aab09489eb9cfe6802c4
Menelisi Mthembu <menelismthembu@gmail.com>
2025-11-18 02:03:08 +0200

    Added GrcpBranchService to bean

--- src/main/java/co/appointment/config/GrcpClientConfig.java
+++ src/main/java/co/appointment/config/GrcpClientConfig.java
@@ -1,8 +1,11 @@
 package co.appointment.config;
 
 import co.appointment.grpc.AuthServiceGrpc;
+import co.appointment.grpc.BranchServiceGrpc;
 import co.appointment.service.grcp.GrcpAuthServiceImpl;
+import co.appointment.service.grcp.GrcpBranchServiceImpl;
 import co.appointment.shared.service.GrcpAuthService;
+import co.appointment.shared.service.GrcpBranchService;
 import net.devh.boot.grpc.client.inject.GrpcClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,5 +19,13 @@ public class GrcpClientConfig {
     @Bean
     public GrcpAuthService grcpAuthService() {
         return new GrcpAuthServiceImpl(authServiceBlockingStub);
+    }
+
+    @GrpcClient("branch-service")
+    BranchServiceGrpc.BranchServiceBlockingStub branchServiceBlockingStub;
+
+    @Bean
+    public GrcpBranchService grcpBranchService() {
+        return new GrcpBranchServiceImpl(branchServiceBlockingStub);
     }
 }
