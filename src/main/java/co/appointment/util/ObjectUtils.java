63dc6c8d111afd439f3e59f5f0d6746be23df9ae
Menelisi Mthembu <menelismthembu@gmail.com>
2025-11-18 02:04:27 +0200

    Added GetBranchResponse parameter

--- src/main/java/co/appointment/util/ObjectUtils.java
+++ src/main/java/co/appointment/util/ObjectUtils.java
@@ -2,9 +2,12 @@ package co.appointment.util;
 
 import co.appointment.entity.Appointment;
 import co.appointment.entity.Slot;
+import co.appointment.grpc.GetBranchResponse;
 import co.appointment.grpc.GetUserResponse;
 import co.appointment.shared.constant.SharedConstants;
+import co.appointment.shared.util.SharedObjectUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.StringUtils;
 
 @Slf4j
 public class ObjectUtils {
@@ -17,6 +20,7 @@ public class ObjectUtils {
      */
     public static String getAppointmentPendingConfirmedEmailBody(final Appointment appointment,
                                                                  final GetUserResponse userResponse,
+                                                                 final GetBranchResponse branchResponse,
                                                                  final String emailTemplate) {
         return String.format(emailTemplate,
                 userResponse.getFullName(),
@@ -29,6 +33,7 @@ public class ObjectUtils {
     }
     public static String getAppointmentConfirmedEmailBody(final Appointment appointment,
                                                           final GetUserResponse userResponse,
+                                                          final GetBranchResponse branchResponse,
                                                           final String emailTemplate) {
         return String.format(emailTemplate,
                 userResponse.getFullName(),
@@ -41,10 +46,11 @@ public class ObjectUtils {
     }
     public static String getAppointmentCancelledEmailBody(final Appointment appointment,
                                                           final GetUserResponse userResponse,
+                                                          final GetBranchResponse branchResponse,
                                                           final String emailTemplate) {
         return String.format(emailTemplate,
                 userResponse.getFullName(),
-                appointment.getBranchId(),
+                branchResponse.getName(),
                 appointment.getAppointmentDate(),
                 getAppointmentSlot(appointment),
                 appointment.getDescription(),
@@ -56,5 +62,25 @@ public class ObjectUtils {
             return "Any time";
         }
         return String.format("%s - %s", appointmentSlot.getSlotStart(), appointmentSlot.getSlotEnd());
+    }
+    private static String appendBranchDetails(final GetBranchResponse response) {
+        String fullAddress = String.format("%s %s %s  %s  %s %s",
+                SharedObjectUtils.returnEmptyIfNullOrBlank(response.getStreetNo()),
+                SharedObjectUtils.returnEmptyIfNullOrBlank(response.getAddressLine1()),
+                SharedObjectUtils.returnEmptyIfNullOrBlank(response.getAddressLine2()),
+                SharedObjectUtils.returnEmptyIfNullOrBlank(response.getCity()),
+                SharedObjectUtils.returnEmptyIfNullOrBlank(response.getProvince()),
+                SharedObjectUtils.returnEmptyIfNullOrBlank(response.getPostalCode()));
+        return String.format("<b>Address:</b>%s<br/><b>Email:</b>%s<br/><b>Land Line:</b>%s<br/><b>Fax:</b>%s",
+                fullAddress,
+                response.getEmailAddress(),
+                response.getLandLine(),
+                response.getFaxNumber());
+    }
+    private static String returnEmptyStringIfNull(final String stringValue) {
+        if(StringUtils.hasText(stringValue)) {
+            return stringValue;
+        }
+        return "";
     }
 }
