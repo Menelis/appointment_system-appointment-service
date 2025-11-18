8e5c25525eae843924d648e58758b1594c9ed67e
Menelisi Mthembu <menelismthembu@gmail.com>
2025-11-18 02:02:12 +0200

    Remove unused annotation

--- src/main/java/co/appointment/service/grcp/GrcpAuthServiceImpl.java
+++ src/main/java/co/appointment/service/grcp/GrcpAuthServiceImpl.java
@@ -4,9 +4,8 @@ package co.appointment.service.grcp;
 import co.appointment.grpc.AuthServiceGrpc;
 import co.appointment.shared.service.GrcpAuthService;
 import co.appointment.shared.service.GrcpAuthServiceBase;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
+
 public class GrcpAuthServiceImpl extends GrcpAuthServiceBase implements GrcpAuthService {
 
     public GrcpAuthServiceImpl(final AuthServiceGrpc.AuthServiceBlockingStub authServiceBlockingStub) {
