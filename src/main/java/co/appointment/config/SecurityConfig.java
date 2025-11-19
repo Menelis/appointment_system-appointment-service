b1275823241524b69a7423ee85bd4ad4e116bd0b
Menelisi Mthembu <menelismthembu@gmail.com>
2025-11-19 02:00:38 +0200

    Added jwtAuthenticationConverter

--- src/main/java/co/appointment/config/SecurityConfig.java
+++ src/main/java/co/appointment/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package co.appointment.config;
 
 import co.appointment.shared.constant.RoleConstants;
+import co.appointment.shared.util.SharedJwtUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -8,6 +9,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Profile("!test")
@@ -26,5 +28,11 @@ public class SecurityConfig {
                         .anyRequest().authenticated())
                 .oauth2ResourceServer((oath2) -> oath2.jwt(Customizer.withDefaults()))
                 .build();
+    }
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter(final AppConfigProperties appConfigProperties) {
+        return SharedJwtUtils.convertJwtGrantedAuthorities(appConfigProperties.getAuthoritiesClaimName(),
+                appConfigProperties.getAuthoritiesClaimPrefix());
     }
 }
