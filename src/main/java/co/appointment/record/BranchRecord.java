d8f67baa8f3116f409099ced61a0f9e47f15e691
Menelisi Mthembu <menelismthembu@gmail.com>
2025-11-28 23:06:40 +0200

    Added more branch properties to be used on the front end

--- src/main/java/co/appointment/record/BranchRecord.java
+++ src/main/java/co/appointment/record/BranchRecord.java
@@ -1,3 +1,3 @@
 package co.appointment.record;
 
-public record BranchRecord(String name) {}
+public record BranchRecord(String name, String streetNo, String addressLine1, String email, String landLine) {}
