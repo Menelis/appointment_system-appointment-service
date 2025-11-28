881790858238fc34398e15219bbe51d2eecfec78
Menelisi Mthembu <menelismthembu@gmail.com>
2025-11-28 23:05:20 +0200

    Removed unused import

--- src/main/java/co/appointment/controller/AppointmentControllerV1.java
+++ src/main/java/co/appointment/controller/AppointmentControllerV1.java
@@ -11,7 +11,6 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
